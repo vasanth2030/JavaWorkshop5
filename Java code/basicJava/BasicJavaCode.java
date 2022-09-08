089680926b63a4f9b29fb4523c6213bcb562a248
vasanth2030 <vasanths2030@gmail.com>
2022-09-08 21:41:21 +0530

    day1

---
+++ Java code/basicJava/BasicJavaCode.java
@@ -0,0 +1,15 @@
+package basicJava;
+
+public class BasicJavaCode {
+	public static void main(String[] args) {
+		String name="Vasanth";
+		int age=20;
+		float height=(float)178.5; //typecasting
+		
+		System.out.println("Hello world");
+		System.out.println("Name: "+name);
+		System.out.println("Age: "+age);
+		System.out.println("Height: "+height+"cm");
+	}
+
+}
