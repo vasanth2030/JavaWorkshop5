e52cf268cec341a476a01e5e9a05debd756d1a48
vasanth2030 <vasanths2030@gmail.com>
2022-09-12 21:28:32 +0530

    day2

--- Java code/basicJava/BasicJavaCode.java
+++ Java code/basicJava/BasicJavaCode.java
@@ -1,11 +1,19 @@
 package basicJava;
 
+import java.util.Scanner;
+
 public class BasicJavaCode {
 	public static void main(String[] args) {
-		String name="Vasanth";
-		int age=20;
-		float height=(float)178.5; //typecasting
+		Scanner sc=new Scanner(System.in);
 		
+		System.out.println("Enter your name: ");
+		String name= sc.next();
+		sc.nextLine();
+		System.out.println("Enter your age: ");
+		int age= sc.nextInt();
+		System.out.println("Enter your height: ");
+		float height= sc.nextFloat();  //(float)178.5 //typecasting
+			
 		System.out.println("Hello world");
 		System.out.println("Name: "+name);
 		System.out.println("Age: "+age);
