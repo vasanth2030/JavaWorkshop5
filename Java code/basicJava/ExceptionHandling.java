a03e198d04f167fbbbaa9ad0cd9caaf3a2ee8ca6
vasanth2030 <vasanths2030@gmail.com>
2022-09-19 21:28:18 +0530

    day6

---
+++ Java code/basicJava/ExceptionHandling.java
@@ -0,0 +1,41 @@
+package basicJava;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionHandling {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter two numbers: ");
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		division(num1, num2);
+		System.out.println("Div: " + div(num1, num2));
+		System.out.println("End of program");
+	}
+
+	public static int div(int a, int b) {
+		int div=-1;
+		try {
+			div = a / b;
+			throw new InputMismatchException("Manually thrown exception");
+		} catch (ArithmeticException e) {
+			System.out.println(e);
+			System.out.println("Enter a non-zero divisor");
+		} catch(Exception ex)
+		{
+			System.out.println(ex);
+			System.out.println("Inside 'Exception' catch block");
+		} finally {
+			System.out.println("Inside finally");
+			return div;
+		}
+	}
+	
+	public static int division(int a, int b) throws ArithmeticException
+	{
+		int div=a/b;
+		return div;
+	}
+
+}
--- Java code/basicJava/Methods.java
+++ Java code/basicJava/Methods.java
@@ -3,7 +3,7 @@ package basicJava;
 import java.util.Scanner;
 
 public class Methods {
-	public static void main(String[] args) {
+	public static void main(String[] args) {  
 		Scanner sc= new Scanner(System.in);
 		System.out.println("Enter two numbers: ");
 		int num1= sc.nextInt();
